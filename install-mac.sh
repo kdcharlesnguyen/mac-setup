de1f233426c047625b57292fae9583185654eac6
kdcharlesnguyen <nguyenduykhangcharles@gmail.com>
2025-03-14 17:52:52 -0400

    Update order of install modules

--- install-mac.sh
+++ install-mac.sh
@@ -1,21 +1,31 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo export PATH=/opt/homebrew/bin:$PATH >> ~/.zshrc
+echo 'export PATH=/opt/homebrew/bin:$PATH' >> ~/.zshrc
+
+source ~/.zshrc
+
+brew install powerlevel10k
+echo 'source /opt/homebrew/share/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
+brew install zsh-autocomplete
+
+# Should be at the top of .zshrc file
+echo 'source /path/to/zsh-autocomplete/zsh-autocomplete.plugin.zsh' >> ~/.zshrc
+
+brew install zsh-autosuggestions
+echo 'source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
 
 brew install git
 brew install maccy
 brew install yt-dlp
 brew install make
 brew install vlc
-brew install obsidian
+brew install rectangle
+brew install alfred
+brew install --cask firefox@developer-edition
 
-brew install zsh-autosuggestions
-echo "source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-
-brew install zsh-autocomplete
-
-# Move this to the top of .zshrc file
-echo "source /path/to/zsh-autocomplete/zsh-autocomplete.plugin.zsh" >> ~/.zshrc
+# Download manually to select silicon version
+# - Notion
 
 echo "# Clean deleted branches
 function gitclean() {
@@ -25,7 +35,4 @@ function gitclean() {
   done
 }" >> ~/.zshrc
 
-brew install powerlevel10k
-echo "source /opt/homebrew/share/powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
-
-alias ytd='yt-dlp -x --audio-quality 0 --audio-format flac --ffmpeg-location $HOME/Downloads/ffmpeg'
+# alias ytd='yt-dlp -x --audio-quality 0 --audio-format flac --ffmpeg-location $HOME/Downloads/ffmpeg'
