e06dd7105b8d00f5e0ea1b88e6be0b512a23bad2
kdcharlesnguyen <nguyenduykhangcharles@gmail.com>
2026-02-04 23:16:48 -0500

    Add readme and applications, formatting, comments

---
+++ README.md
@@ -0,0 +1,16 @@
+# Mac Setup
+
+## Instruction
+Run the following command
+```shell
+./install-mac.sh
+```
+
+## Applications to install 
+5. Atuin
+1. Brave
+2. Docker
+3. Jetbrains Toolbox
+4. Notion
+6. Spotify
+7. Rekordbox
--- install-mac.sh
+++ install-mac.sh
@@ -15,17 +15,24 @@ echo 'source /path/to/zsh-autocomplete/zsh-autocomplete.plugin.zsh' >> ~/.zshrc
 brew install zsh-autosuggestions
 echo 'source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
 
-brew install git
+# Personal
 brew install maccy
 brew install yt-dlp
-brew install make
 brew install vlc
-brew install rectangle
 brew install alfred
-brew install --cask firefox@developer-edition
+brew install rectangle
 
-# Download manually to select silicon version
-# - Notion
+# Developer Tools
+brew install git
+brew install make
+brew install ripgrep
+brew install bat
+brew install curl
+brew install tldr
+brew install wget
+brew install helm
+brew install kind
+brew install kubernetes-cli
 
 echo "# Clean deleted branches
 function gitclean() {
@@ -33,6 +40,11 @@ function gitclean() {
   for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`;
     do git branch -D $branch;
   done
+}" >> ~/.zshrc
+
+echo "# Music downloader
+ytd() {
+  yt-dlp -x --audio-quality 0 --audio-format flac --ffmpeg-location ~/ffmpeg $1
 }" >> ~/.zshrc
 
 # alias ytd='yt-dlp -x --audio-quality 0 --audio-format flac --ffmpeg-location $HOME/Downloads/ffmpeg'
